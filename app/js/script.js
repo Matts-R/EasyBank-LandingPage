dab87d7850d33ad8aa72585467583e5bab0a6fb5
Matts-R <matheushrdasilva13@gmail.com>
2021-01-24 17:50:32 -0300

    Hamburguer menu animation finished

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,18 @@
-console.log("ola");
+const btnHamburguer = $('#btnHamburguer');
+const header = $('.header');
+const overlay = $('.overlay');
+overlay.addClass('fade-out');
+
+btnHamburguer.on('click', function () {
+  if (header.hasClass('open')) {
+    header.removeClass('open');
+
+    overlay.removeClass('fade-in');
+    overlay.addClass('fade-out');
+  } else {
+    header.addClass('open');
+
+    overlay.removeClass('fade-out');
+    overlay.addClass('fade-in');
+  }
+})
