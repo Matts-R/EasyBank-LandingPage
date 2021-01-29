a4506f2241ca197e6e8c9ca234e9b5b9ae598547
Matts-R <matheushrdasilva13@gmail.com>
2021-01-29 20:41:40 -0300

    Responsive menu finished

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,30 @@
+const body = $('body');
 const btnHamburguer = $('#btnHamburguer');
 const header = $('.header');
 const overlay = $('.overlay');
 overlay.addClass('fade-out');
+const menu = $('#mobileMenu');
 
 btnHamburguer.on('click', function () {
   if (header.hasClass('open')) {
     header.removeClass('open');
 
+    body.removeClass('noscroll')
+
     overlay.removeClass('fade-in');
     overlay.addClass('fade-out');
+
+    menu.removeClass('fade-in');
+    menu.addClass('fade-out');
   } else {
     header.addClass('open');
 
+    body.addClass('noscroll')
+
     overlay.removeClass('fade-out');
     overlay.addClass('fade-in');
+
+    menu.removeClass('fade-out');
+    menu.addClass('fade-in');
   }
 })
