089f69503c649365eb10b9973d21701b126cf8b3
Matts-R <matheushrdasilva13@gmail.com>
2021-02-15 20:15:22 -0300

    Adding some responsive layout

--- app/js/script.js
+++ app/js/script.js
@@ -2,29 +2,34 @@ const body = $('body');
 const btnHamburguer = $('#btnHamburguer');
 const header = $('.header');
 const overlay = $('.overlay');
-overlay.addClass('fade-out');
+const image = $('#image');
 const menu = $('#mobileMenu');
+overlay.addClass('fade-out');
 
 btnHamburguer.on('click', function () {
   if (header.hasClass('open')) {
-    header.removeClass('open');
-
     body.removeClass('noscroll')
+
+    header.removeClass('open');
 
     overlay.removeClass('fade-in');
     overlay.addClass('fade-out');
 
     menu.removeClass('fade-in');
     menu.addClass('fade-out');
-  } else {
-    header.addClass('open');
 
+    image.show(800, 'linear')
+  } else {
     body.addClass('noscroll')
+
+    header.addClass('open');
 
     overlay.removeClass('fade-out');
     overlay.addClass('fade-in');
 
     menu.removeClass('fade-out');
     menu.addClass('fade-in');
+
+    image.hide(800, 'linear')
   }
 })
